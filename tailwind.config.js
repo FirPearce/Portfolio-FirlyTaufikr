e980c56e879d1d15f24f65f3510befbacc3a0755
Firpearce <firpearce@Firpearces-Mac-Pro.local>
2024-05-20 20:24:02 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#00224D",
+        darklite: "#667A94",
+      },
+    },
+  },
+  plugins: [],
+};
