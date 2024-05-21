49b515b4b8a60a2b91a544445c5007c7b622a8e3
FirPearce <firlycholeh@gmail.com>
2024-05-21 23:52:49 +0700

    <feat> hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,18 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
     extend: {
       colors: {
         dark: "#00224D",
         darklite: "#667A94",
         primary: "#496989",
+      },
+      screens: {
+        "2xl": "1320px",
       },
     },
   },
